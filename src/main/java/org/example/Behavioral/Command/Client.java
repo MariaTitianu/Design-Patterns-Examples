2f9fe1fcda064fa9512c1cc4a60d4db4de87f3f8
MariaTitianu <maria.titianu.3004@gmail.com>
2023-10-22 17:51:59 +0300

    -_-

--- src/main/java/org/example/Behavioral/Command/Client.java
+++ src/main/java/org/example/Behavioral/Command/Client.java
@@ -3,6 +3,6 @@ package org.example.Behavioral.Command;
 public class Client {
     public static void main(String[] args) {
         Editor editor = new Editor();
-        editor.init();
+        editor.init(); 
     }
 }
